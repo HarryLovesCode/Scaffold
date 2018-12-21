b56fe38b64103de3008483bf21141e4a6a22b15e
Harry Gould <harrison.gould16@gmail.com>
2018-12-21 13:26:23 -0500

    Initial commit

---
+++ Source/Main.cpp
@@ -0,0 +1,209 @@
+#ifdef __EMSCRIPTEN__
+#include <emscripten/emscripten.h>
+#include <emscripten/html5.h>
+#endif
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <iostream>
+#include <vector>
+
+struct Vertices
+{
+    float x, y;
+    float r, g, b;
+};
+
+struct Application
+{
+    GLFWwindow *window;
+    GLuint program;
+    GLint mvp_location;
+};
+
+static const Vertices vertices[3] =
+    {
+        {-0.6f, -0.4f, 1.f, 0.f, 0.f},
+        {0.6f, -0.4f, 0.f, 1.f, 0.f},
+        {0.f, 0.6f, 0.f, 0.f, 1.f}};
+
+static const char *vertex_shader_text =
+    "precision lowp float; \n"
+    "uniform mat4 MVP;\n"
+    "attribute vec3 vCol;\n"
+    "attribute vec2 vPos;\n"
+    "varying vec3 color;\n"
+    "void main()\n"
+    "{\n"
+    "    gl_Position = MVP * vec4(vPos, 0.0, 1.0);\n"
+    "    color = vCol;\n"
+    "}\n";
+
+static const char *fragment_shader_text =
+    "precision lowp float; \n"
+    "varying vec3 color;\n"
+    "void main()\n"
+    "{\n"
+    "    gl_FragColor = vec4(color, 1.0);\n"
+    "}\n";
+
+static void error_callback(int error, const char *description)
+{
+    std::cerr << "[ERROR] GLFW: Failed due to: " << description << std::endl;
+}
+
+static void key_callback(GLFWwindow *window, int key, int scancode, int action, int mods)
+{
+    (void)scancode;
+    (void)mods;
+
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+    }
+}
+
+void draw(void *app)
+{
+    Application *engine = (Application *)app;
+    int width, height;
+
+    glfwGetFramebufferSize(engine->window, &width, &height);
+
+    float ratio = width / (float)height;
+
+    glViewport(0, 0, width, height);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glm::mat4 m = glm::rotate(glm::mat4(1.0), (float)glfwGetTime(), glm::vec3(0.0, 0.0, 1.0));
+    glm::mat4 p = glm::ortho(-ratio, ratio, -1.0f, 1.0f, 1.0f, -1.0f);
+    glm::mat4 mvp = p * m;
+
+    glUseProgram(engine->program);
+    glUniformMatrix4fv(engine->mvp_location, 1, GL_FALSE, glm::value_ptr(mvp));
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glfwSwapBuffers(engine->window);
+    glfwPollEvents();
+}
+
+int main(void)
+{
+    GLFWwindow *window;
+    GLuint vertex_buffer, vertex_shader, fragment_shader, program;
+    GLint mvp_location, vpos_location, vcol_location;
+
+    glfwSetErrorCallback(error_callback);
+
+    if (!glfwInit())
+    {
+        std::cerr << "[ERROR] GLFW: Failed to initialize." << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+
+    window = glfwCreateWindow(640, 480, "Simple example", NULL, NULL);
+
+    if (!window)
+    {
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+
+    glfwSetKeyCallback(window, key_callback);
+    glfwMakeContextCurrent(window);
+
+    GLenum err = glewInit();
+
+    if (err != GLEW_OK)
+    {
+        std::cerr << "[ERROR] GLEW: " << glewGetErrorString(err) << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    glfwSwapInterval(1);
+
+    glGenBuffers(1, &vertex_buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    GLint success = GL_FALSE;
+
+    vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertex_shader, 1, &vertex_shader_text, NULL);
+    glCompileShader(vertex_shader);
+    glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &success);
+
+    if (success == GL_FALSE)
+    {
+        GLint logLength = 0;
+        glGetShaderiv(vertex_shader, GL_INFO_LOG_LENGTH, &logLength);
+
+        std::vector<GLchar> errorLog(logLength);
+        glGetShaderInfoLog(vertex_shader, logLength, NULL, &errorLog[0]);
+
+        std::cout << "Vertex shader: " << &errorLog[0] << std::endl;
+        glDeleteShader(vertex_shader);
+
+        return -1;
+    }
+
+    fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragment_shader, 1, &fragment_shader_text, NULL);
+    glCompileShader(fragment_shader);
+    glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &success);
+
+    if (success == GL_FALSE)
+    {
+        GLint logLength = 0;
+        glGetShaderiv(fragment_shader, GL_INFO_LOG_LENGTH, &logLength);
+
+        std::vector<GLchar> errorLog(logLength);
+        glGetShaderInfoLog(fragment_shader, logLength, NULL, &errorLog[0]);
+
+        std::cout << "Fragment shader: " << &errorLog[0] << std::endl;
+        glDeleteShader(fragment_shader);
+
+        return -1;
+    }
+
+    program = glCreateProgram();
+
+    glAttachShader(program, vertex_shader);
+    glAttachShader(program, fragment_shader);
+    glLinkProgram(program);
+
+    mvp_location = glGetUniformLocation(program, "MVP");
+    vpos_location = glGetAttribLocation(program, "vPos");
+    vcol_location = glGetAttribLocation(program, "vCol");
+
+    glEnableVertexAttribArray(vpos_location);
+    glVertexAttribPointer(vpos_location, 2, GL_FLOAT, GL_FALSE,
+                          sizeof(float) * 5, (void *)0);
+
+    glEnableVertexAttribArray(vcol_location);
+    glVertexAttribPointer(vcol_location, 3, GL_FLOAT, GL_FALSE,
+                          sizeof(float) * 5, (void *)(sizeof(float) * 2));
+
+    Application *app = (Application *)malloc(sizeof(Application));
+
+    app->window = window;
+    app->program = program;
+    app->mvp_location = mvp_location;
+
+#ifndef __EMSCRIPTEN__
+    while (!glfwWindowShouldClose(window))
+    {
+        draw((void*) app);
+    }
+#else
+    emscripten_set_main_loop_arg(draw, (void *)app, 0, 1);
+#endif
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    exit(EXIT_SUCCESS);
+}
